0f7b8cfd6f8645d4f5dc92215b7f93716646941b
Daniel Shapero <shapero.daniel@gmail.com>
2023-01-27 13:03:37 -0800

    Combine linearized + nonlinear action functions

--- demos/ice-shelf/ice-shelf.py
+++ demos/ice-shelf/ice-shelf.py
@@ -19,9 +19,11 @@ args = parser.parse_args()
 ρ_W = Constant(icepack.constants.water_density)
 ρ = ρ_I * (1 - ρ_I / ρ_W)
 g = Constant(icepack.constants.gravity)
-n = Constant(icepack.constants.glen_flow_law)
-T = Constant(260.0)
-A = icepack.rate_factor(T)
+
+# At about -14C, with an applied stress of 100 kPa, the glacier will experience
+# a strain rate of 10 (m / yr) / km at -14C.
+τ = Constant(0.1)  # MPa
+ε = Constant(0.01) # (km / yr) / km
 
 
 def exact_velocity(x, inflow_velocity, inflow_thickness, thickness_change, length):
@@ -30,6 +32,8 @@ def exact_velocity(x, inflow_velocity, inflow_thickness, thickness_change, lengt
     dh = Constant(thickness_change)
     lx = Constant(length)
 
+    n = Constant(icepack.constants.glen_flow_law)
+    A = ε / τ**n
     u_0 = Constant(100.0)
     h_0, dh = Constant(500.0), Constant(100.0)
     ζ = A * (ρ * g * h_0 / 4) ** n
@@ -38,24 +42,22 @@ def exact_velocity(x, inflow_velocity, inflow_thickness, thickness_change, lengt
     return firedrake.as_vector((u_0 + du, 0.0))
 
 
-def linearized_action(z, h, u_in, inflow_ids):
+def action(z, h, u_in, inflow_ids, exponent):
     u, M = firedrake.split(z)
     mesh = z.ufl_domain()
     ν = firedrake.FacetNormal(mesh)
     d = mesh.geometric_dimension()
-    power = h * A / 4 * (inner(M, M) - tr(M) ** 2 / (d + 1)) * dx
-    constraint = inner(u, div(h * M) - 0.5 * ρ * g * grad(h**2)) * dx
-    boundary = h * inner(dot(M, ν), u_in) * ds(inflow_ids)
-    return power + constraint - boundary
 
+    n = Constant(exponent)
+    A = ε / τ**n
 
-def action(z, h, u_in, inflow_ids):
-    u, M = firedrake.split(z)
-    mesh = z.ufl_domain()
-    ν = firedrake.FacetNormal(mesh)
-    d = mesh.geometric_dimension()
     M_2 = (inner(M, M) - tr(M) ** 2 / (d + 1)) / 2
-    power = 2 * h * A / (n + 1) * M_2 ** ((n + 1) / 2) * dx
+    if exponent == 1:
+        M_n = M_2
+    else:
+        M_n = M_2 ** ((n + 1) / 2)
+
+    power = 2 * h * A / (n + 1) * M_n * dx
     constraint = inner(u, div(h * M) - 0.5 * ρ * g * grad(h**2)) * dx
     boundary = h * inner(dot(M, ν), u_in) * ds(inflow_ids)
     return power + constraint - boundary
@@ -71,11 +73,11 @@ def solve_dual_problem(z, *args):
         }
     }
 
-    L = linearized_action(z, *args)
+    L = action(z, *args, exponent=1)
     F = firedrake.derivative(L, z)
     firedrake.solve(F == 0, z, **params)
 
-    L = action(z, *args)
+    L = action(z, *args, exponent=icepack.constants.glen_flow_law)
     F = firedrake.derivative(L, z)
     firedrake.solve(F == 0, z, **params)
 
